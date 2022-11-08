f4e51f4e8b6f3d1b7ef81d5e360d6073ae621bd2
Sercan Noyan Germiyanoğlu <sngermiyanoglu@hotmail.com>
2022-11-08 17:18:20 +0300

    Revise some codes regarding JWT in API Gateway
    Revise some codes regarding JWT in API Gateway

--- apigateway/src/main/java/com/microservice/apigateway/util/JwtUtils.java
+++ apigateway/src/main/java/com/microservice/apigateway/util/JwtUtils.java
@@ -33,16 +33,20 @@ public class JwtUtils {
             return true;
         } catch (SignatureException e) {
             LOGGER.error("JwtUtils | validateJwtToken | Invalid JWT signature: {}", e.getMessage());
+            throw new SignatureException(e.getMessage());
         } catch (MalformedJwtException e) {
             LOGGER.error("JwtUtils | validateJwtToken | Invalid JWT token: {}", e.getMessage());
+            throw new MalformedJwtException(e.getMessage());
         } catch (ExpiredJwtException e) {
             LOGGER.error("JwtUtils | validateJwtToken | JWT token is expired: {}", e.getMessage());
+            throw new ExpiredJwtException(null,null,e.getMessage());
         } catch (UnsupportedJwtException e) {
             LOGGER.error("JwtUtils | validateJwtToken | JWT token is unsupported: {}", e.getMessage());
+            throw new UnsupportedJwtException(e.getMessage());
         } catch (IllegalArgumentException e) {
             LOGGER.error("JwtUtils | validateJwtToken | JWT claims string is empty: {}", e.getMessage());
+            throw new IllegalArgumentException(e.getMessage());
         }
 
-        return false;
     }
 }
