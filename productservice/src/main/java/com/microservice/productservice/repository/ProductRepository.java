1e419527d92db204f913c1e99a0b877d6da7a2ba
Sercan Noyan Germiyanoğlu <sngermiyanoglu@hotmail.com>
2022-11-14 23:36:56 +0300

    Delete product service
    Delete product service

--- productservice/src/main/java/com/microservice/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.microservice.productservice.repository;
-
-import com.microservice.productservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-
-}
