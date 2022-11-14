70e19a9e0489ca792773c4319dc7ddab148ffd54
Sercan Noyan Germiyanoğlu <sngermiyanoglu@hotmail.com>
2022-11-15 02:27:57 +0300

    Revise PaymentControllerTest in payment service
    Revise PaymentControllerTest in payment service

--- paymentservice/src/test/java/com/microservice/paymentservice/controller/PaymentControllerTest.java
+++ paymentservice/src/test/java/com/microservice/paymentservice/controller/PaymentControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -33,6 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest({"server.port=0"})
 @AutoConfigureMockMvc
 @EnableConfigurationProperties
+@ActiveProfiles
 public class PaymentControllerTest {
 
     @RegisterExtension
